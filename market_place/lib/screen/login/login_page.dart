ff331706db1504b4820080a9d613ab68b67f2f7e
Nguyễn Đình Linh <linhnguyendeveloper@maplelabs.co>
2022-12-11 23:50:25 +0700

    login ui

--- market_place/lib/screen/login/login_page.dart
+++ market_place/lib/screen/login/login_page.dart
@@ -11,59 +11,75 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: ColorBase.mainGreenColor,
-      body: Column(
-        children: <Widget>[
-          Text(
-            "welcome to".tr(),
-            style: TextStyleBase.montserratStyle(
-                color: ColorBase.whiteColor, size: 24),
-          ),
-          Text(
-            "login to".tr(),
-            style: TextStyleBase.montserratStyle(
-                color: ColorBase.whiteColor, size: 16),
-          ),
-          TextFormFieldBase(
-            hintText: "email_mobile".tr(),
-          ),
-          TextFormFieldBase(
-            hintText: "password".tr(),
-          ),
-          ElevatorButtonBase(
-            onPressed: () {},
-            text: "login".tr(),
-          ),
-          GestureDetector(
-            onTap: () {},
-            child: Text(
-              "forgot pass".tr(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "welcome to".tr(),
               style: TextStyleBase.montserratStyle(
-                  color: ColorBase.whiteColor, size: 16),
+                  color: ColorBase.whiteColor, size: 24),
             ),
-          ),
-          GestureDetector(
-            onTap: () {},
-            child: RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: "don't have account".tr(),
-                    style: TextStyleBase.montserratStyle(
-                        color: ColorBase.whiteColor, size: 16),
-                  ),
-                  TextSpan(
-                    text: "sign up".tr(),
-                    style: TextStyleBase.montserratStyle(
-                        color: ColorBase.whiteColor,
-                        size: 16,
-                        fontWeight: FontWeight.bold),
-                  )
-                ],
+            Padding(
+              padding: const EdgeInsets.only(top: 60, bottom: 40),
+              child: Text(
+                "login to".tr(),
+                style: TextStyleBase.montserratStyle(
+                    color: ColorBase.whiteColor, size: 16),
               ),
             ),
-          )
-        ],
+            TextFormFieldBase(
+              hintText: "email_mobile".tr(),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15, bottom: 40),
+              child: TextFormFieldBase(
+                hintText: "password".tr(),
+                obscureText: true,
+                suffixIcon: Icons.visibility_off_outlined,
+              ),
+            ),
+            ElevatorButtonBase(
+              onPressed: () {},
+              text: "login".tr(),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30),
+              child: GestureDetector(
+                onTap: () {},
+                child: Text(
+                  "forgot pass".tr(),
+                  style: TextStyleBase.montserratStyle(
+                      color: ColorBase.whiteColor, size: 16),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "don't have account".tr(),
+                      style: TextStyleBase.montserratStyle(
+                          color: ColorBase.whiteColor, size: 16),
+                    ),
+                    TextSpan(
+                      text: "sign up".tr(),
+                      style: TextStyleBase.montserratStyle(
+                          color: ColorBase.whiteColor,
+                          size: 16,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- market_place/lib/screen/widgets/text_field_base.dart
+++ market_place/lib/screen/widgets/text_field_base.dart
@@ -4,23 +4,44 @@ import 'package:home_clean/screen/extentions/style_text_extention.dart';
 
 class TextFormFieldBase extends StatelessWidget {
   final String hintText;
-  const TextFormFieldBase({Key? key, required this.hintText}) : super(key: key);
+  final bool obscureText;
+  final IconData? suffixIcon;
+  const TextFormFieldBase(
+      {Key? key,
+      required this.hintText,
+      this.obscureText = false,
+      this.suffixIcon})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       decoration: InputDecoration(
-        hintText: hintText,
-        hintStyle: TextStyleBase.montserratStyle(
-            color: ColorBase.whiteColor, size: 13),
-        enabledBorder: OutlineInputBorder(
-          borderSide: const BorderSide(
-            width: 1,
-            color: ColorBase.whiteColor,
+          hintText: hintText,
+          hintStyle: TextStyleBase.montserratStyle(
+              color: ColorBase.whiteColor, size: 13),
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              width: 1,
+              color: ColorBase.whiteColor,
+            ),
+            borderRadius: BorderRadius.circular(30),
           ),
-          borderRadius: BorderRadius.circular(30),
-        ),
-      ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              width: 1,
+              color: ColorBase.whiteColor,
+            ),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          suffixIcon: Icon(
+            suffixIcon,
+            color: ColorBase.whiteColor,
+          )),
+      style:
+          TextStyleBase.montserratStyle(color: ColorBase.whiteColor, size: 13),
+      cursorColor: ColorBase.whiteColor,
+      obscureText: obscureText,
     );
   }
 }
