8df745ea675b1b37722e24be7ce6a3e2fbbd0419
Nguyễn Đình Linh <linhnguyendeveloper@maplelabs.co>
2022-12-31 00:55:59 +0700

    update ui

---
+++ music/lib/models/info_album.dart
@@ -0,0 +1,6 @@
+class InfoAlbum {
+  final String image;
+  final String name;
+  final String type;
+  InfoAlbum({required this.image, required this.name, required this.type});
+}
--- music/lib/screen/home_page.dart
+++ music/lib/screen/home_page.dart
@@ -4,26 +4,21 @@ import 'package:music/extension/color_extend.dart';
 import 'package:music/extension/font_extend.dart';
 import 'package:music/widgets/behavior.dart';
 
+import '../models/info_album.dart';
 import '../models/info_singer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double horiScreen = 20;
+
+  @override
   Widget build(BuildContext context) {
-    final List<InfoSinger> singer = [
-      InfoSinger(
-          name: "Ayesha Ruhin",
-          description: "Some Feeling",
-          backgroundColor: const Color(0xff9570FF).withOpacity(0.1)),
-      InfoSinger(
-          name: "Jane Cooper",
-          description: "I didn't know",
-          backgroundColor: const Color(0xff6FCF97).withOpacity(0.1))
-    ];
-
-    const double horiScreen = 20;
-
     return Scaffold(
       backgroundColor: const Color(0xffFFFFFF),
       body: SafeArea(
@@ -55,7 +50,7 @@ class HomePage extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left: horiScreen, bottom: 20),
+              padding: EdgeInsets.only(left: horiScreen, bottom: 20),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
@@ -76,22 +71,10 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(
-              height: 70,
-              width: double.infinity,
-              child: ScrollConfiguration(
-                behavior: MyBehavior(),
-                child: ListView.builder(
-                    itemCount: singer.length,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return itemSinger(index, singer, horiScreen);
-                    }),
-              ),
-            ),
+            _listSinger(),
             Padding(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: horiScreen, vertical: 20),
+              padding:
+                  EdgeInsets.symmetric(horizontal: horiScreen, vertical: 20),
               child: Text(
                 "discover".tr(),
                 style: const TextStyle(
@@ -99,9 +82,37 @@ class HomePage extends StatelessWidget {
                     fontSize: 25,
                     fontFamily: FontBase.montserrat),
               ),
-            )
+            ),
+            _listAlbum(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _listSinger() {
+    final List<InfoSinger> singer = [
+      InfoSinger(
+          name: "Ayesha Ruhin",
+          description: "Some Feeling",
+          backgroundColor: const Color(0xff9570FF).withOpacity(0.1)),
+      InfoSinger(
+          name: "Jane Cooper",
+          description: "I didn't know",
+          backgroundColor: const Color(0xff6FCF97).withOpacity(0.1))
+    ];
+
+    return SizedBox(
+      height: 70,
+      width: double.infinity,
+      child: ScrollConfiguration(
+        behavior: MyBehavior(),
+        child: ListView.builder(
+            itemCount: singer.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return itemSinger(index, singer, horiScreen);
+            }),
       ),
     );
   }
@@ -185,6 +196,83 @@ class HomePage extends StatelessWidget {
               ),
             )
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _listAlbum() {
+    List<InfoAlbum> listAlbum = [
+      InfoAlbum(
+          image: "assets/images/home_img_album_1.png",
+          name: "Out Of My Mine",
+          type: "Dance"),
+      InfoAlbum(
+          image: "assets/images/home_img_album_2.png",
+          name: "Freak In Me",
+          type: "Electronic"),
+      InfoAlbum(
+          image: "assets/images/home_img_album_2.png",
+          name: "Freak In Me",
+          type: "Electronic"),
+      InfoAlbum(
+          image: "assets/images/home_img_album_2.png",
+          name: "Freak In Me",
+          type: "Electronic"),
+    ];
+
+    return SizedBox(
+      width: double.infinity,
+      height: 140,
+      child: ScrollConfiguration(
+        behavior: MyBehavior(),
+        child: ListView.builder(
+          itemCount: 4,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) => Padding(
+            padding: EdgeInsets.only(
+                left: index == 0 ? horiScreen : 0,
+                right: index == 3 ? horiScreen : 15),
+            child: Container(
+                width: 140,
+                height: 140,
+                clipBehavior: Clip.hardEdge,
+                padding: const EdgeInsets.fromLTRB(10, 90, 10, 0),
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(32),
+                  image: DecorationImage(
+                      image: AssetImage(listAlbum[index].image)),
+                ),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 7),
+                  decoration: BoxDecoration(
+                      color: const Color(0xffB7B3B3).withOpacity(0.9),
+                      borderRadius:
+                          const BorderRadius.vertical(top: Radius.circular(8))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        listAlbum[index].name,
+                        style: const TextStyle(
+                            color: Color(0xffFFFFFF),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      const SizedBox(height: 3),
+                      Text(
+                        listAlbum[index].type,
+                        style: TextStyle(
+                            color: const Color(0xffFFFFFF).withOpacity(0.5),
+                            fontSize: 12,
+                            fontWeight: FontWeight.w300),
+                      )
+                    ],
+                  ),
+                )),
+          ),
         ),
       ),
     );
