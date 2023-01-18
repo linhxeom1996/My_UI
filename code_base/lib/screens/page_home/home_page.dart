a35c505470774a252078ae7c21ac6a9ee4e0e2b9
Nguyễn Đình Linh <linhnguyendeveloper@maplelabs.co>
2023-01-18 22:16:10 +0700

    app bar base

--- code_base/lib/screens/page_home/home_page.dart
+++ code_base/lib/screens/page_home/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:code_base/screens/widgets/app_bar_widget.dart';
 import 'package:code_base/screens/widgets/button_widget.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,6 +9,10 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBarCommon(
+        title: "discover".tr(),
+        colorTitle: Colors.black,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
---
+++ code_base/lib/screens/widgets/app_bar_widget.dart
@@ -0,0 +1,48 @@
+import 'package:code_base/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+class AppBarCommon extends StatelessWidget implements PreferredSizeWidget {
+  final Widget? leading;
+  final String? title;
+  final Color? colorTitle;
+  final double? sizeTitle;
+  final FontWeight? fontWeightTitle;
+  final String? fontTitle;
+  final Color? bgColor;
+  final bool? centerTitle;
+  final List<Widget>? actions;
+  const AppBarCommon(
+      {this.leading,
+      this.title = "",
+      this.colorTitle,
+      this.fontTitle,
+      this.fontWeightTitle,
+      this.sizeTitle,
+      this.bgColor,
+      this.centerTitle = true,
+      this.actions,
+      super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: bgColor ?? AppColor.mainColor,
+      centerTitle: centerTitle,
+      actions: actions,
+      leading: leading,
+      title: title != ""
+          ? Text(
+              title!,
+              style: TextStyle(
+                  color: colorTitle,
+                  fontSize: sizeTitle,
+                  fontWeight: fontWeightTitle,
+                  fontFamily: fontTitle),
+            )
+          : const SizedBox(),
+    );
+  }
+}
